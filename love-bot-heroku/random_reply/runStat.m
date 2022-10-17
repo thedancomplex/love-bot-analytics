bb39de66d2073606a75e1ddacf4f6ffd0e6a3c4c
Daniel M. Lofaro <dan@danlofaro.com>
2022-10-17 16:43:30 -0400

    added matlab script, reads the mat scripts made from the python file, plotting the dt between the conversations.  need to plot who respnds fastest

---
+++ love-bot-heroku/random_reply/runStat.m
@@ -0,0 +1,16 @@
+close all
+clear all
+load lovebot20221012Name.mat
+load lovebot20221012Polarity.mat
+load lovebot20221012Subjectivity.mat
+load lovebot20221012Time.mat
+
+dt = diff(lovebotTime);
+
+plot(dt,'.')
+
+ylim([0,200])
+xlabel('Response (n)');
+ylabel('Delay Time (sec)');
+title('Time Difference Between Responses');
+shg
